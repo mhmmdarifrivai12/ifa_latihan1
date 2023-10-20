0e7438cb7094a23e56f67862860f97a570cc4758
Muhammad Arif Rivai <114763633+mhmmdarifrivai12@users.noreply.github.com>
2023-10-20 11:45:40 +0700

    main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,118 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Latihan 1",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan 1"),
-          backgroundColor: Color.fromARGB(255, 255, 191, 0),
-        ),
-        body: Column(
+      home: FormInput(),
+    );
+  }
+}
+
+class FormInput extends StatefulWidget {
+  const FormInput({super.key});
+
+  @override
+  State<FormInput> createState() => _FormInputState();
+}
+
+class _FormInputState extends State<FormInput> {
+  String? _jk;
+  void PilihJk(String value) {
+    setState(() {
+      _jk = value;
+    });
+  }
+
+  List<String> agama = [
+    "Islam",
+    "Kristen Khatolik",
+    "Kristen Protestan",
+    "Hindu",
+    "Budha",
+    "Lainnya",
+  ];
+
+  String _agama = "Islam";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Icon(Icons.list),
+        title: Text("Form Input"),
+        backgroundColor: Colors.teal,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: ListView(
           children: [
-            Text("UNIVERSITAS TEKNOKRAT INDONESIA"),
-            Container(
-              margin: EdgeInsets.all(5),
-              height: 30,
-              width: 500,
-              color: Colors.red,
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Nama Lengkap",
+                labelText: "Nama Lengkap",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
             ),
-            Row(
-              children: [
-                Text("Muhammad"),
-                Text(" Arif"),
-                Text(" Rivai"),
-              ],
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: "Password",
+                labelText: "Password",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            TextField(
+              maxLines: 3,
+              maxLength: 100,
+              decoration: InputDecoration(
+                hintText: "Alamat",
+                labelText: "Alamat",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            RadioListTile(
+              value: "Laki-laki",
+              title: Text("Laki-laki"),
+              groupValue: _jk,
+              onChanged: (String? value) {
+                PilihJk(value!);
+              },
+              activeColor: Colors.teal,
+              subtitle: Text("Pilih ini jika laki-laki"),
+            ),
+            RadioListTile(
+              value: "Perempuan",
+              title: Text("Perempuan"),
+              groupValue: _jk,
+              onChanged: (String? value) {
+                PilihJk(value!);
+              },
+              activeColor: Colors.teal,
+              subtitle: Text("Pilih ini jika perempuan"),
             ),
           ],
         ),
